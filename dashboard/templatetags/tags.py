1df6f705234ac1b99fb45728ee16281af164df5b
naveen <naveenkumar3a5@gmail.com>
2019-12-09 11:24:34 +0530

    Dec9th

---
+++ dashboard/templatetags/tags.py
@@ -0,0 +1,34 @@
+from django import template
+from datetime import date,timedelta
+
+
+register = template.Library()
+
+
+@register.filter(name='status')
+def  filter_status(value):
+	if value==1:
+		return 'Active'
+	else:
+		return 'InActive'	
+
+
+@register.filter(name='is_staff')
+def  filter_staff(value):
+	if value==1:
+		return 'True'
+	else:
+		return 'False'	
+
+
+@register.filter(name="get_title")
+def  get_title(value,args):
+
+	for arg in args:
+		if value is arg.id:
+			return arg.name
+
+
+@register.simple_tag
+def define(val=None):
+  return val
